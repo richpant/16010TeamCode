14a67143e1ca1e6056bf35312f37718aab04cb39
richpant <47926579+richpant@users.noreply.github.com>
2021-10-28 07:47:57 -0500

    changed mechanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MechaDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MechaDrive.java
@@ -56,30 +56,30 @@ public class MechaDrive extends OpMode {
     }
     //-----------------------DRIVE------------------
     public void drive() {
-        double left;
-        double right;
-        double rf = -gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
-        double rr = -gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
-        double lf = gamepad1.right_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
-        double lr = gamepad1.right_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
-        if (Math.abs(rf) >= 0.1 || Math.abs(rr) >= 0.1 || Math.abs(lf) >= 0.1 || Math.abs(lr) >= 0.1) {
-            rightFront.setPower(rf);
-            rightRear.setPower(rr);
-            leftRear.setPower(lr);
-            leftFront.setPower(lf);
-        } else {
-            rightFront.setPower(0);
-            rightRear.setPower(0);
-            leftRear.setPower(0);
-            leftFront.setPower(0);
-        }
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        leftFront.setPower(frontLeftPower);
+        leftRear.setPower(backLeftPower);
+        rightFront.setPower(frontRightPower);
+        rightRear.setPower(backRightPower);
     }
         //-----------------------------------CARRY------------
         private void carousel () {
             if (gamepad2.a)
-                carry.setPower(0.6);
+                carry.setPower(0.5);
             else if (gamepad2.y)
-                carry.setPower(-0.6);
+                carry.setPower(-0.5);
             else
                 carry.setPower(0.0);
         }
